3d1cdda850bc0adc5ed60ce45669186f8a3b4270
Michael Berkovich <theiceberk@gmail.com>
2014-04-12 13:00:56 -0700

    added tests

--- src/test/java/com/tr8n/core/Tr8nTest.java
+++ src/test/java/com/tr8n/core/Tr8nTest.java
@@ -16,12 +16,55 @@ public class Tr8nTest extends BaseTest {
             Tr8n.translate("Hello World")
         );
 
-//        Tr8n.setCurrentLocale("ru");
-//        Assert.assertEquals(
-//                "Привет Мир",
-//                Tr8n.tr("Hello World")
-//        );
+        Tr8n.beginBlockWithOptions(Utils.buildMap("source", "sample"));
+        
+        Assert.assertEquals(
+            "Hello World",
+            Tr8n.translate("Hello World")
+        );
+        
+        Tr8n.endBlock();
 
+        Assert.assertEquals(
+                "You have 5 messages",
+                Tr8n.translate("You have {count||message}", Utils.buildMap("count", 5))
+            );
+
+        Assert.assertEquals(
+                "You have 1 message",
+                Tr8n.translate("You have {count|| one: message, other: messages}", Utils.buildMap("count", 1))
+            );
+
+        Assert.assertEquals(
+                "You have 5 messages",
+                Tr8n.translate("You have {count|| one: message, other: messages}", Utils.buildMap("count", 5))
+            );
+        
+        Assert.assertEquals(
+                "You have <strong>5 messages</strong>",
+                Tr8n.translate("You have [indent: {count||message}]", Utils.buildMap(
+                		"count", 5,
+                		"indent", "<strong>{$0}</strong>"
+                ))
+            );
+
+        Assert.assertEquals(
+                "<strong>You</strong> have <strong>5 messages</strong>",
+                Tr8n.translate("[indent: You] have [indent: {count||message}]", Utils.buildMap(
+                		"count", 5,
+                		"indent", "<strong>{$0}</strong>"
+                ))
+            );
+
+        Assert.assertEquals(
+                "<strong>You have <i>5 messages</i></strong> in your mailbox",
+                Tr8n.translate("[bold: You have [italic: {count||message}]] in your mailbox", Utils.buildMap(
+                		"count", 5,
+                		"bold", "<strong>{$0}</strong>",
+                		"italic", "<i>{$0}</i>"
+                ))
+            );
+        
     }
 
 }
